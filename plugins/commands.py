dec1d68836c89b6ed24d06ecc895d3ae53296676
PRIME BOT'z <sahidhasannayem@gmail.com>
2025-04-15 17:19:06 +0600

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -238,7 +238,7 @@ async def start(client, message):
                 buttons = [
                     [InlineKeyboardButton("♻️ ᴄʟɪᴄᴋ ʜᴇʀᴇ ᴛᴏ ᴠᴇʀɪꜰʏ ♻️", url=verify)],
                     [InlineKeyboardButton("⁉️ ʜᴏᴡ ᴛᴏ ᴠᴇʀɪꜰʏ ⁉️", url=howtodownload)],
-                    [InlineKeyboardButton('💎 ᴜᴘɢʀᴀᴅᴇ ᴛᴏ ᴘʀᴇᴍɪᴜᴍ 💎 •', callback_data="premium")]
+                    [InlineKeyboardButton('• ᴜᴘɢʀᴀᴅᴇ ᴘʟᴀɴ •', callback_data="premium")]
                 ]
 
                 msg = (
