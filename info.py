29df9a531d9db82698f23fb16501c04b0d360bc3
PRIME BOT'z <sahidhasannayem@gmail.com>
2025-04-16 08:15:44 +0600

    Update info.py

--- info.py
+++ info.py
@@ -82,7 +82,7 @@ TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "43200"))
 THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "43200"))
 
 #Othes
-TMDB_API = environ.get("TMDB_API", "ef6c6375cae3e583606e8ac21c964a88")
+TMDB_API = environ.get("TMDB_API", "6abcb6bb99fb77f33c37016a28866ed2")
 MOVIE_UPDATE_NOTIFICATION = bool(environ.get("MOVIE_UPDATE_NOTIFICATION", True))
 NO_RESULTS_MSG = bool(environ.get("NO_RESULTS_MSG", True))
 MAX_B_TN = environ.get("MAX_B_TN", "8")
